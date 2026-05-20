5c7306ed756a5081ae4dfbacc45ead8212371047
GroundliftMediaGmbH <it@groundlift.de>
2026-05-20 16:53:09 +0200

    Add files via upload

---
+++ groundlift_kino_shift_signup/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Groundlift Kino Dienstplan Anmeldung",
+    "summary": "Monatliche Kino-Schichtabfrage mit Website-Eintragung und Erinnerungen",
+    "version": "19.0.1.5.0",
+    "category": "Human Resources",
+    "author": "Groundlift Media GmbH",
+    "license": "LGPL-3",
+    "depends": ["base", "mail", "hr", "website"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/kino_shift_views.xml",
+        "views/website_templates.xml",
+        "data/ir_cron.xml",
+    ],
+    "application": True,
+    "installable": True,
+}
