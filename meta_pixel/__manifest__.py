2e41c51fe2d2827073d8ea9c07b0b4bf41a64d69
GroundliftMediaGmbH <it@groundlift.de>
2026-09-12 19:24:30 +0200

    Add files via upload

---
+++ meta_pixel/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "Meta Pixel",
+    "version": "19.0.1.0.0",
+    "summary": "Event-specific Meta Pixel and Conversions API tracking with reporting",
+    "category": "Marketing/Events",
+    "author": "Groundlift",
+    "license": "LGPL-3",
+    "depends": [
+        "event_sale",
+        "website_event_sale",
+        "website_sale",
+        "payment",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/meta_pixel_config_views.xml",
+        "views/meta_pixel_log_views.xml",
+        "views/event_event_views.xml",
+        "views/res_config_settings_views.xml",
+        "views/website_templates.xml",
+        "views/menu_views.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "meta_pixel/static/src/js/meta_pixel_frontend.js",
+        ],
+    },
+    "application": True,
+    "installable": True,
+}
---
+++ meta_pixel/models/__init__.py
@@ -0,0 +1,5 @@
+from . import meta_pixel_config
+from . import meta_pixel_log
+from . import event_event
+from . import res_config_settings
+from . import payment_transaction
