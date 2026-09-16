53863543fbaf86001461d7579391836f47b9cf21
GroundliftMediaGmbH <it@groundlift.de>
2026-09-16 10:59:31 +0200

    Add files via upload

--- gl_project_checklist/__manifest__.py
+++ gl_project_checklist/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Groundlift Projekt-Checkliste",
     "summary": "Checklisten-Tabs mit Plan-Zeichnungen und Medientechnik direkt im Odoo-Projekt",
-    "version": "19.0.1.2.0",
+    "version": "19.0.1.2.1",
     "category": "Project",
     "author": "Groundlift Media GmbH",
     "website": "https://www.groundlift.de",
